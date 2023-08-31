98d1d8625281e9121447c4240469c257cb228fc5
Connor Pryor <cfpryor@ucsc.edu>
2023-08-31 14:56:46 -0700

    Added first pass at roadr detr neupsl wrapper model.

--- logger.py
+++ logger.py
@@ -1,11 +1,13 @@
 import logging
+import sys
+
 
 def initLogging(logging_level = logging.INFO):
     """
     Initializes the logging format and level.
     """
 
-    logging.basicConfig(format='%(levelname)s - %(asctime)s - %(message)s', level=logging_level)
+    logging.basicConfig(stream=sys.stdout, format='%(levelname)s - %(asctime)s - %(message)s', level=logging_level)
 
 def updateLoggingLevel(logging_level):
     """
--- models/roadr_detr_neupsl.py
+++ models/roadr_detr_neupsl.py
@@ -1,23 +1,37 @@
 #!/usr/bin/env python3
+import logging
 import os
 import sys
 
-import numpy
+import pslpython.deeppsl.model
 import torch.nn
 import torchvision
 
-import pslpython.deeppsl.model
+from torch.utils.data import DataLoader
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import logger
 import utils
 
+from data.roadr_dataset import RoadRDataset
 from models.detr import DETR
 from models.losses import binary_cross_entropy
 from models.losses import pairwise_generalized_box_iou
 from models.hungarian_match import hungarian_match
 
+logger.initLogging(logging_level = logging.DEBUG)
+
+THIS_DIR = os.path.dirname(os.path.realpath(__file__))
+DATA_DIR = os.path.join(THIS_DIR, "..", "data")
+DATA_FILE_NAME = 'road_trainval_v1.0.json'
+
+LABELED_VIDEOS = ["2014-07-14-14-49-50_stereo_centre_01",
+                  "2015-02-03-19-43-11_stereo_centre_04",
+                  "2015-02-24-12-32-19_stereo_centre_04"]
+
+MAX_FRAMES = 40
+TUBE_SIZE = 4
 
 class RoadRDETRNeuPSL(pslpython.deeppsl.model.DeepModel):
     """
@@ -26,10 +40,23 @@ class RoadRDETRNeuPSL(pslpython.deeppsl.model.DeepModel):
     def __init__(self):
         super().__init__()
         self.model = None
+        self.optimizer = None
+        self.lr_scheduler = None
         self.predictions = None
         self.training_data = None
+        self.current_batch = None
+
+        self.bce_weight = 1
+        self.giou_weight = 2
+
+        self.dataset = RoadRDataset(LABELED_VIDEOS, os.path.join(DATA_DIR, DATA_FILE_NAME), max_frames=MAX_FRAMES)
+
+        self.train_dataloader = DataLoader(self.dataset, batch_size=4, shuffle=False)
+        self.train_iterator = iter(self.train_dataloader)
 
     def internal_init_model(self, application, options={}):
+        logging.info("Initializing neural model for application: {0}".format(application))
+
         resnet34 = torchvision.models.resnet34(weights=None)
 
         # Remove the last two layers of the resnet34 model.
@@ -49,43 +76,60 @@ class RoadRDETRNeuPSL(pslpython.deeppsl.model.DeepModel):
         )
         self.model = DETR(backbone, transformer).to(utils.get_torch_device())
 
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=1e-4, weight_decay=1e-4)
+        self.lr_scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=200, gamma=0.1)
+
         return {}
 
     def internal_fit(self, data, gradients, options={}):
-        return {}
+        self.optimizer.zero_grad(set_to_none=True)
+
+        results = self._compute_loss()
+
+        total_loss = self.bce_weight * results["bce_loss"] + self.giou_weight * results["giou_loss"]
+        total_loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
+
+        self.optimizer.step()
+
+        results["total_loss"] = total_loss.item()
+
+        return results
 
     def internal_predict(self, data, options={}):
-        results = {}
-        if options["learn"]:
-            results['mode'] = 'learning'
-            self.model.train()
-        else:
-            results['mode'] = 'inference'
-            self.model.eval()
+        self.current_batch = next(self.train_iterator)
+        frames, train_images, labels, boxes = self.current_batch
 
-        self.predictions = self.model(data)
+        self.predictions = self.model(train_images)
+        predictions = torch.cat((self.predictions["class_probabilities"], self.predictions["boxes"]), dim=2)
+        predictions = predictions.view(-1, predictions.shape[-1])
 
-        return self.predictions, results
+        return predictions.tolist(), {}
 
     def internal_eval(self, data, options={}):
-        frames, train_images, labels, boxes = data
-
         results = {}
+
         if options["learn"]:
-            # Compute the training loss.
-            # For the training loss, we need to first compute the matching between the predictions and the ground truth.
-            matching = hungarian_match(self.predictions["boxes"], boxes)
-
-            # Compute the classification loss using the matching.
-            bce_loss = binary_cross_entropy(self.predictions["class_probabilities"], labels, matching)
-
-            # Compute the bounding box loss using the matching.
-            giou_loss = pairwise_generalized_box_iou(self.predictions["boxes"], boxes, matching)
-
-            results["bce_loss"] = bce_loss
-            results["giou_loss"] = giou_loss
+            results = self._compute_loss()
 
         return results
 
     def internal_save(self, options={}):
         return {}
+
+    def _compute_loss(self):
+        results = {}
+
+        frames, train_images, labels, boxes = self.current_batch
+
+        # Compute the matching between the predictions and the ground truth.
+        matching = hungarian_match(self.predictions["boxes"], boxes)
+
+        # Compute the classification loss using the matching.
+        results["bce_loss"] = binary_cross_entropy(self.predictions["class_probabilities"], labels, matching)
+
+        # Compute the bounding box loss using the matching.
+        results["giou_loss"] = pairwise_generalized_box_iou(self.predictions["boxes"], boxes, matching)
+
+        return results
