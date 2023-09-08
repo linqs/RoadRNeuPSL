Fixed major bug in data loading. Added filtering detections using non-maximum suppression and began implementation of mean average precision.

data/roadr_dataset.py
@@ -1,7 +1,8 @@
 import json
 import logging
-import numpy as np
 import os
+
+import numpy as np
 import torch
 import torchvision.transforms as transforms
 import torchvision.io
@@ -97,6 +98,7 @@ class RoadRDataset(Dataset):
             transforms.Normalize(mean=IMAGE_MEAN, std=IMAGE_STD)])
 
         self.load_data()
+        self.frame_ids_mapping = {frame_id: i for i, frame_id in enumerate(self.frame_ids.tolist())}
 
     def load_data(self):
         logging.info("Loading raw json data: {0}".format(self.data_path))
@@ -118,11 +120,11 @@ class RoadRDataset(Dataset):
 
             logging.debug("Counting valid frames for video: {0} Start frame: {1} End frame: {2}".format(videoname, start_frame, end_frame))
 
-            for frame_name in sorted(database[videoname]['frames'].keys())[start_frame:end_frame]:
+            for frame_name in sorted([int(frame_name) for frame_name in database[videoname]['frames'].keys()])[start_frame:end_frame]:
                 if self.max_frames != 0 and num_video_frames >= self.max_frames:
                     break
 
-                frame = database[videoname]['frames'][frame_name]
+                frame = database[videoname]['frames'][str(frame_name)]
 
                 if "annos" not in frame:
                     continue
@@ -132,9 +134,9 @@ class RoadRDataset(Dataset):
         logging.debug("Total frames counted in all videos: {0}".format(num_frames))
 
         # TODO(Charles): By pre allocating the max bounding boxes per frame, we are wasting a lot of memory.
-        #  Additionally, in evaluation we will have bounding boxes that are all zeros.
+        # Additionally, in evaluation we will have bounding boxes that are all zeros.
         self.frames = np.empty(shape=(num_frames, 2), dtype=object)  # (video_id, frame_id)
-        self.frame_ids = torch.arange(num_frames, dtype=torch.int64)  # (video_id, frame_id)
+        self.frame_ids = torch.arange(num_frames, dtype=torch.int64)
         self.images = torch.empty(size=(num_frames, 3, int(IMAGE_HEIGHT * self.image_resize), int(IMAGE_WIDTH * self.image_resize)), dtype=torch.float32)
         self.labels = torch.empty(size=(num_frames, self.num_queries, NUM_CLASSES + 1), dtype=torch.float32)
         self.boxes = torch.empty(size=(num_frames, self.num_queries, 4), dtype=torch.float32)
@@ -151,11 +153,11 @@ class RoadRDataset(Dataset):
             end_frame = int(len(database[videoname]['frames']) * self.end_frame_percentage)
             logging.debug("Loading valid frames for video: {0} Start frame: {1} End frame: {2}".format(videoname, start_frame, end_frame))
 
-            for frame_name in sorted(database[videoname]['frames'].keys())[start_frame:end_frame]:
+            for frame_name in sorted([int(frame_name) for frame_name in database[videoname]['frames'].keys()])[start_frame:end_frame]:
                 if self.max_frames != 0 and num_video_frames >= self.max_frames:
                     break
 
-                frame = database[videoname]['frames'][frame_name]
+                frame = database[videoname]['frames'][str(frame_name)]
 
                 if "annos" not in frame:
                     continue
@@ -164,7 +166,7 @@ class RoadRDataset(Dataset):
                     torchvision.io.read_image(
                         os.path.join(THIS_DIR, "../data/rgb-images", videoname, "{0:05d}.jpg".format(frame['rgb_image_id']))
                     ).type(torch.float32))
-                self.frames[frame_index] = [videoname, str(frame['rgb_image_id'])]
+                self.frames[frame_index] = [videoname, "{0:05d}.jpg".format(frame['rgb_image_id'])]
 
                 # Extract labels and box coordinate for each box in the frame.
                 frame_labels = torch.zeros(size=(self.num_queries, NUM_CLASSES + 1), dtype=torch.float32)
@@ -173,8 +175,8 @@ class RoadRDataset(Dataset):
                     frame_boxes[bounding_box_index] = torch.tensor(frame['annos'][bounding_box]['box'])
                     frame_labels[bounding_box_index, -1] = 1  # Set the last class to 1 to indicate that there is an object box here.
                     for label_type in LABEL_TYPES:
-                        for label_id in frame_labels[bounding_box_index][frame['annos'][bounding_box][label_type + '_ids']]:
-                            if label_id not in LABEL_MAPPING[label_type]:
+                        for label_id in frame['annos'][bounding_box][label_type + '_ids']:
+                            if int(label_id) not in LABEL_MAPPING[label_type]:
                                 continue
                             frame_labels[bounding_box_index][LABEL_MAPPING[label_type][int(label_id)][0]] = True
 

experiments/task1_evaluate.py
@@ -15,9 +15,12 @@ from data.roadr_dataset import RoadRDataset
 from experiments.task1_pretrain import TASK_NAME
 from experiments.task1_pretrain import task_1_model
 from models.trainer import Trainer
+from models.evaluation import filter_detections
+from models.evaluation import load_labels
 from utils import BASE_RESULTS_DIR
 from utils import EVALUATION_METRICS_FILENAME
-from utils import EVALUATION_PREDICTION_FILENAME
+from utils import EVALUATION_PREDICTION_JSON_FILENAME
+from utils import EVALUATION_PREDICTION_PKL_FILENAME
 from utils import TRAIN_VALIDATION_DATA_PATH
 from utils import TRAINED_MODEL_DIR
 from utils import TRAINED_MODEL_FILENAME
@@ -28,10 +31,39 @@ VALID_VIDEOS = ["2014-06-26-09-53-12_stereo_centre_02",
                 "2015-02-13-09-16-26_stereo_centre_02"]
 
 
+def create_task_1_output_format(dataset, dataloader, class_predictions, box_predictions):
+    output_dict = {}
+
+    all_ids = []
+    all_class_labels = []
+    all_box_labels = []
+
+    for indexes, frames, labels, boxes in dataloader:
+        frames = [dataset.get_frame_and_video_names(index).tolist() for index in indexes]
+        all_ids.extend(frames)
+        all_class_labels.extend(labels.cpu().tolist())
+        all_box_labels.extend(boxes.cpu().tolist())
+
+    for id, class_label, box_label, class_prediction, box_prediction in zip(all_ids, all_class_labels, all_box_labels, class_predictions, box_predictions):
+        if id[0] not in output_dict:
+            output_dict[id[0]] = {}
+
+        output_dict[id[0]][id[1]] = []
+        for prediction, box in zip(class_prediction, box_prediction):
+            # if max(prediction[:-1]) > prediction[-1]:
+            if prediction[-1] < 0.16:
+                output_dict[id[0]][id[1]].append({"labels": prediction[:-1], "bbox": box})
+
+    return output_dict, all_class_labels, all_box_labels
+
+
 def evaluate_dataset(dataset, arguments):
-    if os.path.isfile(os.path.join(arguments.output_dir, EVALUATION_PREDICTION_FILENAME)):
-        logging.info("Skipping evaluation for %s, already exists." % (os.path.join(arguments.output_dir, EVALUATION_PREDICTION_FILENAME),))
+    if os.path.isfile(os.path.join(arguments.output_dir, EVALUATION_PREDICTION_JSON_FILENAME)) and \
+            os.path.isfile(os.path.join(arguments.output_dir, EVALUATION_PREDICTION_PKL_FILENAME)):
+        logging.info("Skipping evaluation for %s, already exists." % (arguments.output_dir,))
         return
+
+    os.makedirs(os.path.join(arguments.output_dir), exist_ok=True)
 
     dataloader = DataLoader(dataset, batch_size=arguments.batch_size, shuffle=False)
 
@@ -40,10 +72,22 @@ def evaluate_dataset(dataset, arguments):
     model.load_state_dict(torch.load(arguments.saved_model_path))
 
     logging.info("Evaluating model.")
-    trainer = Trainer(model, None, None, utils.get_torch_device(), os.path.join(BASE_RESULTS_DIR, TASK_NAME, TRAINED_MODEL_DIR))
-    predictions = trainer.evaluate(dataloader)
+    trainer = Trainer(model, None, None, utils.get_torch_device(), os.path.join(arguments.output_dir))
 
-    print(predictions)
+    frame_indexes, box_predictions, class_predictions = trainer.evaluate(dataloader)
+
+    filtered_detections = filter_detections(torch.Tensor(frame_indexes), torch.Tensor(box_predictions), torch.Tensor(class_predictions)[..., :-1], 0.1)
+    labels = load_labels(dataset, filtered_detections)
+
+    # mean_average_precision = mean_average_precision(filtered_detections, dataset, 0.5)
+
+    # output_dict, all_class_labels, all_box_labels = create_task_1_output_format(dataset, dataloader, class_predictions, box_predictions)
+    #
+    # logging.info("Saving pkl predictions to %s" % os.path.join(arguments.output_dir, EVALUATION_PREDICTION_PKL_FILENAME))
+    # utils.write_pkl_file(os.path.join(arguments.output_dir, EVALUATION_PREDICTION_PKL_FILENAME), output_dict)
+    #
+    # logging.info("Saving json predictions to %s" % os.path.join(arguments.output_dir, EVALUATION_PREDICTION_JSON_FILENAME))
+    # utils.write_json_file(os.path.join(arguments.output_dir, EVALUATION_PREDICTION_JSON_FILENAME), output_dict)
 
 
 def calculate_metrics(dataset, arguments):
@@ -52,10 +96,6 @@ def calculate_metrics(dataset, arguments):
         return
 
     logging.info("Calculating metrics.")
-
-    logging.info("Loading Predictions: %s" % os.path.join(arguments.output_dir, EVALUATION_PREDICTION_FILENAME))
-    # predictions = utils.load_json_file(os.path.join(arguments.output_dir, EVALUATION_PREDICTION_FILENAME))
-
 
 
 def main(arguments):

models/trainer.py
@@ -7,7 +7,7 @@ from models.hungarian_match import hungarian_match
 from models.losses import binary_cross_entropy
 from models.losses import pairwise_generalized_box_iou
 from models.model_utils import save_model_state
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from typing import Tuple, List
 
 from utils import TRAINING_CONVERGENCE_FILENAME
@@ -107,7 +107,9 @@ class Trainer:
         """
         self.model.eval()
 
-        predictions = []
+        all_box_predictions = []
+        all_class_predictions = []
+        all_frame_indexes = []
         total_loss = 0
 
         if torch.cuda.is_available():
@@ -120,10 +122,15 @@ class Trainer:
                 # Transfer the batch to the GPU.
                 batch = [b.to(self.device) for b in batch]
 
+                frame_indexes, images, labels, boxes = batch
+
                 loss = self._compute_loss(batch).item()
                 total_loss += loss
 
-                predictions.extend(self.model(batch[1]))
+                predictions = self.model(images)
+                all_box_predictions.extend(predictions['boxes'].cpu().tolist())
+                all_class_predictions.extend(predictions['class_probabilities'].cpu().tolist())
+                all_frame_indexes.extend(frame_indexes.cpu().tolist())
 
                 tq.set_postfix(loss=loss)
 
@@ -139,7 +146,7 @@ class Trainer:
             training_summary_file.write("{:.5f}, {:.5f}, {:d}".format(
                 total_loss / len(dataloader), total_time, max_gpu_mem))
 
-        return predictions
+        return all_frame_indexes, all_box_predictions, all_class_predictions
 
     def compute_training_loss(self, batch: (Tuple, List), bce_weight: int = 1, giou_weight: int = 2) -> torch.Tensor:
         """
@@ -173,10 +180,10 @@ class Trainer:
         :param giou_weight: The weight to apply to the generalized IoU loss. Default is 2 from DETR paper.
         :return: The training loss for the provided batch.
         """
-        frames, train_images, labels, boxes = data
+        frame_ids, images, labels, boxes = data
 
         # Compute the predictions for the provided batch.
-        predictions = self.model(train_images)
+        predictions = self.model(images)
 
         # Compute the training loss.
         # For the training loss, we need to first compute the matching between the predictions and the ground truth.