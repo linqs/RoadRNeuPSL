Fixed training loss to be average. Added to evaluation. Changed hyperparameters for pretraining. Added analysis that saves images with bounding boxes.

data/roadr_dataset.py
@@ -96,7 +96,7 @@ class RoadRDataset(Dataset):
         self.video_id_frame_id_to_frame_index = {}
 
         self.transforms = transforms.Compose([
-            transforms.Resize(size=(int(IMAGE_HEIGHT * self.image_resize), int(IMAGE_WIDTH * self.image_resize)), antialias=True),
+            transforms.Resize(size=(self.image_height(), self.image_width()), antialias=True),
             transforms.Normalize(mean=IMAGE_MEAN, std=IMAGE_STD)])
 
         self.load_data()
@@ -138,7 +138,7 @@ class RoadRDataset(Dataset):
         # Additionally, in evaluation we will have bounding boxes that are all zeros.
         self.frames = np.empty(shape=(num_frames, 2), dtype=object)  # (video_id, frame_id)
         self.frame_ids = torch.arange(num_frames, dtype=torch.int64)
-        self.images = torch.empty(size=(num_frames, 3, int(IMAGE_HEIGHT * self.image_resize), int(IMAGE_WIDTH * self.image_resize)), dtype=torch.float32)
+        self.images = torch.empty(size=(num_frames, 3, self.image_height(), self.image_width()), dtype=torch.float32)
         self.labels = torch.empty(size=(num_frames, self.num_queries, NUM_CLASSES + 1), dtype=torch.float32)
         self.boxes = torch.empty(size=(num_frames, self.num_queries, 4), dtype=torch.float32)
 
@@ -193,6 +193,12 @@ class RoadRDataset(Dataset):
 
     def get_frame_and_video_names(self, frame_id):
         return self.frames[frame_id]
+
+    def image_height(self):
+        return int(IMAGE_HEIGHT * self.image_resize)
+
+    def image_width(self):
+        return int(IMAGE_WIDTH * self.image_resize)
 
     def __len__(self):
         return len(self.frames)

experiments/task1_evaluate.py
@@ -12,12 +12,13 @@ import utils
 from torch.utils.data import DataLoader
 
 from data.roadr_dataset import RoadRDataset
-from experiments.task1_pretrain import TASK_NAME
 from experiments.task1_pretrain import task_1_model
-from models.trainer import Trainer
+from experiments.task1_pretrain import TASK_NAME
+from models.analysis import save_images_with_bounding_boxes
 from models.evaluation import filter_detections
 from models.evaluation import load_ground_truth_for_detections
 from models.evaluation import mean_average_precision
+from models.trainer import Trainer
 from utils import BASE_RESULTS_DIR
 from utils import EVALUATION_METRICS_FILENAME
 from utils import EVALUATION_PREDICTION_JSON_FILENAME
@@ -31,8 +32,8 @@ VALID_VIDEOS = ["2014-06-26-09-53-12_stereo_centre_02",
                 "2014-11-25-09-18-32_stereo_centre_04",
                 "2015-02-13-09-16-26_stereo_centre_02"]
 
-CONFIDENCE_THRESHOLD = 0.00
-
+CONFIDENCE_THRESHOLD = 0.50
+IOU_THRESHOLD = 0.50
 
 def create_task_1_output_format(dataset, frame_indexes, class_predictions, box_predictions):
     output_dict = {}
@@ -46,7 +47,9 @@ def create_task_1_output_format(dataset, frame_indexes, class_predictions, box_p
         output_dict[frame_id[0]][frame_id[1]] = []
         for prediction, box in zip(class_prediction, box_prediction):
             if prediction[-1] >= CONFIDENCE_THRESHOLD:
-                output_dict[frame_id[0]][frame_id[1]].append({"labels": prediction, "bbox": box})
+                output_dict[frame_id[0]][frame_id[1]].append({"labels": prediction[:-1], "bbox": box})
+            else:
+                output_dict[frame_id[0]][frame_id[1]].append({"labels": [0.0] * len(prediction[:-1]), "bbox": box})
 
     return output_dict
 
@@ -107,10 +110,10 @@ def calculate_metrics(dataset, arguments):
     frame_indexes, class_predictions, box_predictions = format_saved_predictions(predictions, dataset)
 
     logging.info("Calculating metrics.")
-    filtered_detections, filtered_detection_indexes = filter_detections(torch.Tensor(frame_indexes), torch.Tensor(box_predictions), torch.Tensor(class_predictions)[..., :-1], 0.5)
+    filtered_detections, filtered_detection_indexes = filter_detections(torch.Tensor(frame_indexes), torch.Tensor(box_predictions), torch.Tensor(class_predictions), IOU_THRESHOLD)
     filtered_detections_ground_truth = load_ground_truth_for_detections(dataset, filtered_detection_indexes)
 
-    mean_avg_prec = mean_average_precision(filtered_detections_ground_truth, filtered_detections, 0.5)
+    mean_avg_prec = mean_average_precision(filtered_detections_ground_truth, filtered_detections, IOU_THRESHOLD)
     logging.info("Mean average precision: %s" % mean_avg_prec)
 
     logging.info("Saving metrics to %s" % os.path.join(arguments.output_dir, EVALUATION_METRICS_FILENAME))
@@ -128,8 +131,14 @@ def main(arguments):
 
     dataset = RoadRDataset(VALID_VIDEOS, TRAIN_VALIDATION_DATA_PATH, arguments.image_resize, arguments.num_queries, max_frames=arguments.max_frames)
 
+    logging.info("Evaluating dataset.")
     evaluate_dataset(dataset, arguments)
+
+    logging.info("Calculating metrics.")
     calculate_metrics(dataset, arguments)
+
+    logging.info("Saving images with bounding boxes.")
+    save_images_with_bounding_boxes(dataset, arguments)
 
 
 def _load_args():

models/evaluation.py
@@ -3,59 +3,8 @@ import torch
 from torchmetrics.detection import MeanAveragePrecision
 from torchvision.ops import nms
 
-# CONFIDENCE_THRESHOLD = 0.025
-CONFIDENCE_THRESHOLD = 0.000
+CONFIDENCE_THRESHOLD = 0.025
 NUM_CLASSES = 41
-
-
-def mean_average_precision(ground_truths, detections, iou_threshold=0.5):
-    """
-    Computes the mean average precision (mAP) for a given set of predictions and ground truths.
-    :param ground_truths: List containing the ground truths.
-    :param detections: List containing the detections.
-    :param iou_threshold: Threshold for the IoU.
-    """
-    map_metric = MeanAveragePrecision(iou_thresholds=[iou_threshold], iou_type="bbox")
-
-    map_metric.update(detections, ground_truths)
-    values = map_metric.compute()
-
-    return float(values['map'])
-
-
-def load_ground_truth_for_detections(dataset, indexes, num_classes=NUM_CLASSES):
-    """
-    Loads the ground truth labels for the given detections.
-    :param dataset: Dataset for which the labels should be loaded.
-    :param indexes: List of indexes for which the detections were computed.
-    :param num_classes: Number of classes in the dataset.
-    """
-    ground_truth = []
-
-    for frame_index in indexes:
-        frame_truth_boxes = dataset[frame_index][3]
-        frame_truth_labels = dataset[frame_index][2]
-
-        mask_frame_truth_boxes = frame_truth_boxes.sum(dim=1).gt(0)
-
-        frame_truth_boxes = frame_truth_boxes[mask_frame_truth_boxes]
-        frame_truth_labels = frame_truth_labels[mask_frame_truth_boxes]
-
-        for class_index in range(num_classes):
-            class_frame_truth_labels = frame_truth_labels[:, class_index]
-
-            mask_class_frame_truth_labels = class_frame_truth_labels.gt(0)
-
-            if class_frame_truth_labels.sum() == 0:
-                ground_truth.append({'boxes': torch.Tensor([]), 'labels': torch.Tensor([])})
-                continue
-
-            class_frame_truth_boxes = frame_truth_boxes[mask_class_frame_truth_labels]
-            class_frame_frame_labels = torch.Tensor([int(class_index)] * len(class_frame_truth_boxes)).type(torch.int64)
-
-            ground_truth.append({'boxes': class_frame_truth_boxes, 'labels': class_frame_frame_labels})
-
-    return ground_truth
 
 
 def filter_detections(frame_indexes, box_predictions, class_predictions, iou_threshold, num_classes=NUM_CLASSES):
@@ -96,3 +45,53 @@ def filter_detections(frame_indexes, box_predictions, class_predictions, iou_thr
             filtered_detections.append({'boxes': filtered_frame_boxes, 'scores': filtered_frame_scores, 'labels': filtered_frame_labels})
 
     return filtered_detections, filtered_detection_indexes
+
+
+def load_ground_truth_for_detections(dataset, indexes, num_classes=NUM_CLASSES):
+    """
+    Loads the ground truth labels for the given detections.
+    :param dataset: Dataset for which the labels should be loaded.
+    :param indexes: List of indexes for which the detections were computed.
+    :param num_classes: Number of classes in the dataset.
+    """
+    ground_truth = []
+
+    for frame_index in indexes:
+        frame_truth_boxes = dataset[frame_index][3]
+        frame_truth_labels = dataset[frame_index][2]
+
+        mask_frame_truth_boxes = frame_truth_boxes.sum(dim=1).gt(0)
+
+        frame_truth_boxes = frame_truth_boxes[mask_frame_truth_boxes]
+        frame_truth_labels = frame_truth_labels[mask_frame_truth_boxes]
+
+        for class_index in range(num_classes):
+            class_frame_truth_labels = frame_truth_labels[:, class_index]
+
+            mask_class_frame_truth_labels = class_frame_truth_labels.gt(0)
+
+            if class_frame_truth_labels.sum() == 0:
+                ground_truth.append({'boxes': torch.Tensor([]), 'labels': torch.Tensor([])})
+                continue
+
+            class_frame_truth_boxes = frame_truth_boxes[mask_class_frame_truth_labels]
+            class_frame_frame_labels = torch.Tensor([int(class_index)] * len(class_frame_truth_boxes)).type(torch.int64)
+
+            ground_truth.append({'boxes': class_frame_truth_boxes, 'labels': class_frame_frame_labels})
+
+    return ground_truth
+
+
+def mean_average_precision(ground_truths, detections, iou_threshold=0.5):
+    """
+    Computes the mean average precision (mAP) for a given set of predictions and ground truths.
+    :param ground_truths: List containing the ground truths.
+    :param detections: List containing the detections.
+    :param iou_threshold: Threshold for the IoU.
+    """
+    map_metric = MeanAveragePrecision(iou_thresholds=[iou_threshold], iou_type="bbox")
+
+    map_metric.update(detections, ground_truths)
+    values = map_metric.compute()
+
+    return float(values['map'])

models/trainer.py
@@ -7,7 +7,7 @@ from models.hungarian_match import hungarian_match
 from models.losses import binary_cross_entropy
 from models.losses import pairwise_generalized_box_iou
 from models.model_utils import save_model_state
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader
 from typing import Tuple, List
 
 from utils import TRAINING_CONVERGENCE_FILENAME
@@ -39,7 +39,6 @@ class Trainer:
         validation_score = float("inf")
         best_validation_score = float("inf")
         best_loss = 0
-        loss_value = 0
         total_time = 0
 
         if torch.cuda.is_available():
@@ -47,6 +46,7 @@ class Trainer:
 
         for epoch in tqdm.tqdm(range(n_epochs), "Training Model", leave=True):
             epoch_start_time = time.time()
+            epoch_loss = 0
 
             with tqdm.tqdm(training_dataloader) as tq:
                 tq.set_description("Epoch:{}".format(epoch))
@@ -60,11 +60,11 @@ class Trainer:
 
                     loss.backward()
                     self.post_gradient_computation()
-                    loss_value = loss.item()
+                    epoch_loss += loss.item()
 
                     self.optimizer.step()
 
-                    tq.set_postfix(loss=loss_value, validation_score=validation_score)
+                    tq.set_postfix(loss=epoch_loss / ((step + 1) * training_dataloader.batch_size), validation_score=validation_score)
 
                 self.scheduler.step()
 
@@ -73,14 +73,14 @@ class Trainer:
 
                 if validation_score < best_validation_score:
                     best_validation_score = validation_score
-                    best_loss = loss_value
+                    best_loss = epoch_loss / (len(training_dataloader) * training_dataloader.batch_size)
                     save_model_state(self.model, self.out_directory)
 
             epoch_time = time.time() - epoch_start_time
             total_time += epoch_time
 
             learning_convergence += "{:.5f}, {:.5f}, {:.5f}, {:.5f}, {:.5f} \n".format(
-                total_time, epoch_time, loss.item(), validation_score, best_validation_score)
+                total_time, epoch_time, epoch_loss / (len(training_dataloader) * training_dataloader.batch_size), validation_score, best_validation_score)
 
         if torch.cuda.is_available():
             max_gpu_mem = torch.cuda.max_memory_allocated()
@@ -170,7 +170,7 @@ class Trainer:
             validation_batch = [b.to(self.device) for b in validation_batch]
 
             validation_score += self._compute_loss(validation_batch).item()
-        return validation_score / len(validation_data)
+        return validation_score / (len(validation_data) * validation_data.batch_size)
 
     def _compute_loss(self, data: (Tuple, List), bce_weight: int = 1, giou_weight: int = 2) -> torch.Tensor:
         """