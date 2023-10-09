d44dbdf5e3d9c77d4f21fd769dee72e6f35d45d5
Connor Pryor <cfpryor@ucsc.edu>
2023-10-09 11:57:35 -0700

    Added three additional neupsl models.

--- data/generate_psl_data.py
+++ data/generate_psl_data.py
@@ -21,6 +21,17 @@ AGENT_CLASSES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 BOUNDING_BOX_CLASSES = [41, 42, 43, 44, 45]
 
 
+def load_constraints(path):
+    constraints = []
+    raw_constraints = utils.load_csv_file(path)
+    raw_constraints = [row[1:] for row in raw_constraints[1:]]
+    for index_i in range(len(raw_constraints)):
+        for index_j in range(len(raw_constraints)):
+            constraints.append([index_i, index_j, int(raw_constraints[index_i][index_j])])
+
+    return constraints
+
+
 def generate_experiment(experiment_dir, tube_size):
     utils.make_dir(experiment_dir)
 
@@ -38,23 +49,26 @@ def generate_experiment(experiment_dir, tube_size):
         bounding_box_classes.append([bounding_box_class])
 
     entity_targets = []
+    linked_frames = []
     for tube_index in range(tube_size):
+        if tube_index < tube_size - 1:
+            linked_frames.append([tube_index, tube_index + 1])
+            linked_frames.append([tube_index + 1, tube_index])
+
         for bounding_box_index in range(NUM_NEUPSL_QUERIES):
             for class_index in range(NUM_CLASSES + len(BOUNDING_BOX_CLASSES)):
                 entity_targets.append([tube_index, bounding_box_index, class_index])
 
-    hard_co_occurrence = []
-    raw_hard_co_occurrence = utils.load_csv_file(os.path.join(THIS_DIR, "constraints", "hard-co-occurrence.csv"))
-    raw_hard_co_occurrence = [row[1:] for row in raw_hard_co_occurrence[1:]]
-    for index_i in range(len(raw_hard_co_occurrence)):
-        for index_j in range(len(raw_hard_co_occurrence)):
-            hard_co_occurrence.append([index_i, index_j, int(raw_hard_co_occurrence[index_i][index_j])])
+    hard_co_occurrence = load_constraints(os.path.join(THIS_DIR, "constraints", "hard-co-occurrence.csv"))
+    soft_co_occurrence = load_constraints(os.path.join(THIS_DIR, "constraints", "soft-co-occurrence.csv"))
 
     utils.write_psl_file(os.path.join(experiment_dir, "entity-data-map.txt"), entity_data_map)
     utils.write_psl_file(os.path.join(experiment_dir, "classes-agent.txt"), agent_classes)
     utils.write_psl_file(os.path.join(experiment_dir, "classes-bounding-box.txt"), bounding_box_classes)
     utils.write_psl_file(os.path.join(experiment_dir, "entity-targets.txt"), entity_targets)
+    utils.write_psl_file(os.path.join(experiment_dir, "linked-frame.txt"), linked_frames)
     utils.write_psl_file(os.path.join(experiment_dir, "hard-co-occurrence.txt"), hard_co_occurrence)
+    utils.write_psl_file(os.path.join(experiment_dir, "soft-co-occurrence.txt"), soft_co_occurrence)
 
 
 def _load_args():
